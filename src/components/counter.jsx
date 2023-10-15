e79f828929d41fca3086e356db64f1337d586d3c
MeriemBelh <meriem.balhaddad2001@gmail.com>
2023-10-16 00:43:20 +0100

    counterCompon

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,49 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    counter: 0,
+    value: this.props.counter.value,
   };
 
+  // we use arrow functions to avoid doing this for all methods that we need to bind
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
   render() {
+    console.log("props", this.props);
     return (
-      <div>
-        <span>{this.state.counter}</span>
-        <button>Increment</button>
-      </div>
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </React.Fragment>
     );
+  }
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
